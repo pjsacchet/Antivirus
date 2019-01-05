10e62111f3180337f6679f0435f53dd35cf8771c
Patrick Sacchet <psacchet8282@gmail.com>
2019-01-05 17:56:19 -0500

    Update 01/05/19

--- siggrep.py
+++ siggrep.py
@@ -5,15 +5,49 @@
 # Step 1: Implement Yara recognition to correctly identify malicious files based on given signature files
 
 import sys
+import os
 import yara
 
 # Function wil;l recursively search through the files in the users directory
 # @param: user_dir - Directory path of the user files
-def dir_rec_search(user_dir)
-    
+def dir_rec_search(user_dir):
+    if(os.path.isdir(user_dir)):
+        try:
+            for file in os.listdir(user_dir):
+                print ("Filename: " + file)
+                new_dir = user_dir + "/" + file
+                print("Attempting to access: " + new_dir)
+                dir_rec_search(new_dir)
+        except:
+            print("Not allowed access apparently")
+    else:
+        for file in os.listdir(user_dir):
+            print("File found: " + file)
 
 
-
+# Function will detect the system configuration of the user and return a string representing said os
+# @return: OS type - Will identify User's OS type and return a string representation
+def get_os_type():
+    os = sys.platform
+    if(os == "win32"):
+        print("Platform detedted: Windows")
+        print("Executing commands... ")
+        return "windows"
+    if(os == "linux"):
+        print("Platform detected: Linux")
+        print("Executing commands... ")
+        return "linux"
+    if(os == "darwin"):
+        print("Platform detected: Mac")
+        print("Executing commands... ")
+        return "mac"
+    if(os == "cygwin"):
+        print("Platform detected: Windows/Cygwin")
+        print("Executing commands... ")
+        return "cygwin"
+    else:
+        print("Platform not detected, exiting...")
+        sys.exit()
 
 
 
@@ -25,7 +59,13 @@ def dir_rec_search(user_dir)
 def main():
     #Will recursively go through user files initially
         # Grab user direcotry, call recursive directory search function, compare using yara rules file(s)
-
+    #print ("Current working directory: " + os.getcwd())
+    print("Attempting to detect your system configuration... ")
+    os = get_os_type()
+    if (os == "windows"):
+        dir = "C:\\"
+        print (dir)
+        dir_rec_search(dir)
 
 
 
