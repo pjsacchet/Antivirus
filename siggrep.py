a69bae12b25c87a1b8d0e28b25a071ecee0d60d1
Patrick Sacchet <psacchet8282@gmail.com>
2019-01-04 23:53:39 -0500

    Initial siggrep file

---
+++ siggrep.py
@@ -0,0 +1,32 @@
+# @author - Patrick Sacchet
+# @version - 1.0 - 01/04/2019
+# Goal - Implement anitvirus usserface that will recursively go through user directories and use Yara signature files to identify malicious files
+# How:
+# Step 1: Implement Yara recognition to correctly identify malicious files based on given signature files
+
+import sys
+import yara
+
+# Function wil;l recursively search through the files in the users directory
+# @param: user_dir - Directory path of the user files
+def dir_rec_search(user_dir)
+    
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    #Will recursively go through user files initially
+        # Grab user direcotry, call recursive directory search function, compare using yara rules file(s)
+
+
+
+
+main()
