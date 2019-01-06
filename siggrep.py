d439026c8d1527f3b741afbb4bf2a7e6fd72693e
Patrick Sacchet <psacchet8282@gmail.com>
2019-01-05 22:32:00 -0500

    Updated searching user directory functionality

--- siggrep.py
+++ siggrep.py
@@ -2,27 +2,34 @@
 # @version - 1.0 - 01/04/2019
 # Goal - Implement anitvirus usserface that will recursively go through user directories and use Yara signature files to identify malicious files
 # How:
-# Step 1: Implement Yara recognition to correctly identify malicious files based on given signature files
+# Step 1: Implement proper directory searching functionality based on OS
+# Step 2: Implement Yara recognition to correctly identify malicious files based on given signature files
 
 import sys
 import os
+import time
 import yara
 
-# Function wil;l recursively search through the files in the users directory
-# @param: user_dir - Directory path of the user files
-def dir_rec_search(user_dir):
-    if(os.path.isdir(user_dir)):
-        try:
-            for file in os.listdir(user_dir):
-                print ("Filename: " + file)
-                new_dir = user_dir + "/" + file
-                print("Attempting to access: " + new_dir)
-                dir_rec_search(new_dir)
-        except:
-            print("Not allowed access apparently")
-    else:
-        for file in os.listdir(user_dir):
-            print("File found: " + file)
+
+
+def yara_sig_check(file):
+    print("Will check file with rule file(s)")
+
+
+# Function will search through user's entire computer, checking files appropiately
+def dir_search(user_dir):
+    timer_start = time.time()
+    file_number = 0
+    for root, dirs, files in os.walk(user_dir, topdown=True):
+        for name in files:
+            print("File found: " + name)
+            # Call function to check Yara signature
+            file_number += 1
+    timer_end = time.time()
+    total_time = timer_end - timer_start
+    print("Time taken to scan whole system: " + str(total_time))
+    print("Total files found: " + str(file_number))
+    # Windows (Command: dir /s /a-d c:\) found 600836 files, this found 584755 files ---> Is this truly finding ALL files?
 
 
 # Function will detect the system configuration of the user and return a string representing said os
@@ -32,29 +39,26 @@ def get_os_type():
     if(os == "win32"):
         print("Platform detedted: Windows")
         print("Executing commands... ")
+        time.sleep(3)
         return "windows"
     if(os == "linux"):
         print("Platform detected: Linux")
         print("Executing commands... ")
+        time.sleep(3)
         return "linux"
     if(os == "darwin"):
         print("Platform detected: Mac")
         print("Executing commands... ")
+        time.sleep(3)
         return "mac"
     if(os == "cygwin"):
         print("Platform detected: Windows/Cygwin")
         print("Executing commands... ")
+        time.sleep(3)
         return "cygwin"
     else:
         print("Platform not detected, exiting...")
         sys.exit()
-
-
-
-
-
-
-
 
 def main():
     #Will recursively go through user files initially
@@ -64,8 +68,7 @@ def main():
     os = get_os_type()
     if (os == "windows"):
         dir = "C:\\"
-        print (dir)
-        dir_rec_search(dir)
+        dir_search(dir)
 
 
 
