6e50461e0886fbba958281b721c0bc790d4dffe2
Patrick Sacchet <pjsacchet@loyola.edu>
2019-01-19 17:58:31 -0500

    Mutli Signature File Search Functionality

--- siggrep.py
+++ siggrep.py
@@ -12,21 +12,37 @@ import time
 import yara
 
 
+def write_file():
+    return;
+
+
+
+
+# Purpose of this function will be to create a dictionary of Yara rule files to feed to the Yara compile funciton
+# @param: rule_path - Path to the directory of all rule files
+# @return: Dictionary containing all Yara rule files
+def mk_dict(rule_path):
+    # Will take the directory containing all signature files anc create a dictionary with the keys as the filenames, and values as their path values
+    rule_dict = {}
+    for file in os.listdir(rule_path):
+        filepath = os.path.join(rule_path, file)
+        rule_dict[file] = filepath
+    return rule_dict
+
+
 # Function will open the filepath to the Yara rule files, compile them, and check to see if they match any of the files
 # @param:  file - file to check match for with Yara rules
-# @return: None (printing result/writing to log)
-def yara_sig_check(file, rule_path):
+# @param: rules - rules object compiled via Yara
+# @return: file if hit
+def yara_sig_check(file, rules):
     try:
         ### Need something for accessing files with restrictions on access ###
-        ### Compile Yara rule files (if multiple files, I need to add to a dict) ###
-        rules = yara.compile(filepath = rule_path)
         # Will scan the file for 60 seconds, any longer it will move on to the next file
         matches = rules.match(file, timeout = 60)
         if (len(matches) > 0):
             # Grab proper filename not directory
             filename = os.path.splitext(os.path.basename(file))[0]
             print("File was hit: " + filename + " with rule: " + str(matches[0]))
-            #time.sleep(1)
             return file
     except:
         print("Seems like there was an error with permissions")
@@ -34,17 +50,20 @@ def yara_sig_check(file, rule_path):
 
 # Function will search through user's entire computer, checking files appropriately
 # @param: user_dir - Directory of the user's OS (should be base directory so we can search entire system)
+# @param: rule_dict - Dictionary containing all rule files for Yara to compile
 # @return: None (printing information on files, time to search, and scanning each file with Yara signatures
-def dir_search(user_dir, rule_dir):
+def dir_search(user_dir, rule_dict):
     hit_files = []
     timer_start = time.time()
     file_number = 0
+    # Compile Yara rules before starting scan
+    rules = yara.compile(filepaths = rule_dict)
     for root, dirs, files in os.walk(user_dir, topdown=True):
         for name in files:
             print("Scanning file: " + name)
             # Call function to check file with Yara signatures
             file_path = os.path.realpath(os.path.join(root, name))
-            scanned_file = (yara_sig_check(file_path, rule_dir))
+            scanned_file = (yara_sig_check(file_path, rules))
             # Don't add the file to the list if it wasn't flagged/was already added
             if(scanned_file != None and hit_files.__contains__(scanned_file) == False):
                 scanned_file = os.path.splitext(os.path.basename(scanned_file))[0]
@@ -68,15 +87,16 @@ def get_rule_dir(os_type):
         rule_path = r"C:\Users\Patrick\rule_files"
         return rule_path
     if (os_type == "darwin"):
-        rule_path = "/Users/patricksacchet/PycharmProjects/Antivrus/rule_files/rules"
+        rule_path = "/Users/patricksacchet/PycharmProjects/Antivrus/rule_files/"
         return rule_path
     if(os_type.startswith("linux")):
-        rule_path = "/home/pjsacchet/PycharmProjects/Antivrus/rule_files/rules"
+        rule_path = "/home/pjsacchet/PycharmProjects/Antivrus/rule_files/"
         return rule_path
     return;
 
 
 # Function will detect the system configuration of the user and return a string representing said os
+# @param: None
 # @return: OS type - Will identify User's OS type and return a string representation
 def get_os_type():
     os = sys.platform
@@ -106,23 +126,29 @@ def get_os_type():
 
 
 def main():
-    # Will find user's type of OS and search through entire system, recording number of files and time taken to scan
+    # Will find user's type of OS and search through entire system, recording number of files, time taken to scan and files that were hit
     # Should I compile rules as this runs? --> Check for updates on server then compile new rules
     print("Attempting to detect your system configuration... ")
+    time.sleep(3)
     os = get_os_type()
+    output_file =
+    # For any OS, get the proper rule path, create a dictionary with the rule files, and search through the user's computer
     if (os == "windows"):
         dir = "C:\\"
         rule_path = get_rule_dir(os)
-        dir_search(dir, rule_path)
+        rule_dict = mk_dict(rule_path)
+        dir_search(dir, rule_dict)
     if (os == "mac"):
         dir = "/"
         rule_path = get_rule_dir(os)
-        dir_search(dir, rule_path)
+        rule_dict = mk_dict(rule_path)
+        dir_search(dir, rule_dict)
     # Only want to search my folder on school computers
     if (os == "linux"):
         dir = "/home/pjsacchet"
         rule_path = get_rule_dir(os)
-        dir_search(dir, rule_path)
+        rule_dict = mk_dict(rule_path)
+        dir_search(dir, rule_dict)
 
 
 if __name__ == "__main__":
