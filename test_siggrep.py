cf9cac32560e0df9a2b086d7fdfc8d6624478839
Patrick Sacchet <pjsacchet@loyola.edu>
2019-07-28 22:48:20 -0400

    Starting unit tests for siggrep

--- test_siggrep.py
+++ test_siggrep.py
@@ -3,16 +3,15 @@
 # Implementing unit tests for all functions within siggrep.py
 import coverage
 import unittest
-import siggrep.py
+import siggrep
 import os
-
-
-
-
-
 
 class SiggrepTest(unittest.TestCase):
 
-    def testwrite_file(self, filename, string):
-        write_file("testfile", "this is a test")
-        self.assertTrue(os.path.exists("testfile.txt") == True)
+    def testwrite_file(self):
+        siggrep.write_file("testfile", "this is a test")
+        self.assertTrue(os.path.exists("testfile") == True)
+
+
+if __name__ == '__main__':
+    unittest.main()
